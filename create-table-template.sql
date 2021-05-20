fe888a0b9c52e09a014d39a5da7a6cff7c14ccd0
mario <jmuspeed@gmail.com>
2021-05-20 16:33:55 -0500

    version 0.2

--- create-table-template.sql
+++ create-table-template.sql
@@ -1,5 +1,5 @@
 create table t_user(
-    user_id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    user_id varchar(50) not null primary key,
     user_name varchar(50) NOT NULL,
     user_surnames varchar(50)NOT NULL,
     user_address varchar(50)NOT NULL,
@@ -16,19 +16,19 @@ create table t_user(
     user_status int
 );
 create table t_document_type(
-    document_type_id int not null primary key AUTO_INCREMENT ,
+    document_type_id varchar(50) not null primary key,
     document_type_code varchar(10),
     document_type_name varchar(20) COMMENT 'DNI, PASAPORTE',
     document_type_status int
 );
 create table t_vehicle_type(
-    vehicle_type_id int not null primary key AUTO_INCREMENT ,
+    vehicle_type_id varchar(50) not null primary key,
     vehicle_type_name varchar(20),
     vehicle_type_image varchar(100),
     vehicle_type_status int
 );
 create table t_driver(
-    driver_id int not null primary key AUTO_INCREMENT ,
+    driver_id varchar(50) not null primary key,
     driver_name varchar(50),
     driver_surnames varchar(50),
     driver_number_document varchar(20),
@@ -49,7 +49,7 @@ create table t_driver(
     FOREIGN KEY (id_vehicle_type)REFERENCES t_vehicle_type(vehicle_type_id)
 );
 create table t_soat(
-    soat_id int not null primary key AUTO_INCREMENT ,
+    soat_id varchar(50) not null primary key,
     soat_code varchar(20),
     soat_image varchar(100),
     soat_description text,
@@ -58,7 +58,7 @@ create table t_soat(
     FOREIGN KEY (id_driver)REFERENCES t_driver(driver_id)
 );
 create table t_license(
-    license_id int not null primary key AUTO_INCREMENT ,
+    license_id varchar(50) not null primary key,
     license_code varchar(20),
     license_image varchar(100),
     license_description text,
@@ -68,7 +68,7 @@ create table t_license(
     
 );
 create table t_bussines_category(
-    bussines_category_id int not null primary key AUTO_INCREMENT ,
+    bussines_category_id varchar(50) not null primary key,
     bussines_category_name varchar(50),
     bussines_category_image varchar(100),
     bussines_category_description varchar(100),
@@ -76,7 +76,7 @@ create table t_bussines_category(
     bussines_category_order int
 );
 create table t_bussines(
-    bussines_id int not null primary key AUTO_INCREMENT ,
+    bussines_id varchar(50) not null primary key,
     bussines_code varchar(100),
     bussines_owner varchar(100),
     bussines_name varchar(100),
@@ -96,17 +96,21 @@ create table t_bussines(
 
 );
 create table t_ingredient(
-    ingredient_id int not null primary key AUTO_INCREMENT,
+    ingredient_id varchar(50) not null primary key,
     ingredient_name varchar(50),
     ingredient_image varchar(100),
     ingredient_status int
 );
-create table t_type_product(
-    t_type_product_id int not null primary key AUTO_INCREMENT,
-    t_type_product_name varchar(50) COMMENT ' BEBIDA, PLATO, PROMOCION, COMBO'
+create table t_product_category(
+    t_product_category_id varchar(50) not null primary key,
+    t_product_category_name varchar(50) COMMENT ' BEBIDA, JUGO, CEVICHE',
+    t_product_category_image varchar(100),
+    t_product_category_desdcription varchar(100),
+    t_product_category_status int,
+    t_prodcut_category_order int,
 );
 create table t_product(
-    product_id int not null primary key AUTO_INCREMENT ,
+    product_id varchar(50) not null primary key,
     product_name varchar(50),
     product_price double,
     pruduct_image varchar(100),
@@ -115,13 +119,13 @@ create table t_product(
     product_score double,
     product_status int,
     id_bussines int,
-    id_type_product int,
+    id_product_category int,
     FOREIGN KEY (id_bussines)REFERENCES t_bussines(bussines_id),
-    FOREIGN KEY (id_type_product)REFERENCES t_type_product(t_type_product_id)
+    FOREIGN KEY (id_product_category)REFERENCES t_product_category(product_category_id)
 
 );
 create table t_producto_ingredient(
-    product_ingredient_id int not null primary key AUTO_INCREMENT,
+    product_ingredient_id varchar(50) not null primary key,
     product_ingredient_porcent double,
     product_ingredient_description text,
     id_ingredient int,
@@ -130,42 +134,43 @@ create table t_producto_ingredient(
     FOREIGN KEY (id_product)REFERENCES t_product(product_id)
 );
 create table t_offer(
-    offer_id int not null primary key AUTO_INCREMENT,
+    offer_id varchar(50) not null primary key,
     offer_name varchar(50),
     offer_price double,
     offer_image varchar(100),
     offer_description text,
+    offer_type varchar(20) DEFAULT 'OFERTA',
     offer_date_start DATETIME ,
     offer_date_end DATETIME ,
     ofeer_score double,
     offer_status int,
-    id_type_product int,   
-    FOREIGN KEY (id_type_product)REFERENCES t_type_product(t_type_product_id)
+    id_bussines int,   
+    FOREIGN KEY (id_bussines)REFERENCES t_bussines(bussines_id)
 );
 create table t_product_offer(
-    t_product_offer_id int not null primary key AUTO_INCREMENT,
+    t_product_offer_id varchar(50) not null primary key,
     id_offer int,
     id_product int,
     FOREIGN KEY (id_offer)REFERENCES t_offer(offer_id),
     FOREIGN KEY (id_product)REFERENCES t_product(product_id)
 );
 create table t_type_payment(
- type_payment_id int not null primary key AUTO_INCREMENT,
+ type_payment_id varchar(50) not null primary key,
  type_payment_name varchar(20),
  type_payment_image varchar(100),
  type_payment_status int
 );
 create table t_order_status(
-    order_status_id int not null primary key AUTO_INCREMENT,
+    order_status_id varchar(50) not null primary key,
     order_status_name varchar(50) comment 'PEDIDO, PENDIENTE, COCINA, EN CAMINO, RESIVIDO, PAGADO, CANCELADO' 
 );
 create table t_role(
-    role_id int not null primary key AUTO_INCREMENT,
+    role_id varchar(50) not null primary key,
     role_name varchar(50) COMMENT 'ADMIN, CAJA,ETC.',
     role_permision JSON
 );
 create table t_people(
-    people_id int not null primary key AUTO_INCREMENT,
+    people_id varchar(50) not null primary key,
     people_name varchar(50),
     people_email varchar(50),
     people_password varchar(100),
@@ -179,7 +184,7 @@ create table t_people(
 
 );
 create table t_order(
-    order_id int not null primary key AUTO_INCREMENT,
+    order_id varchar(50) not null primary key,
     order_date datetime,
     order_adress_bussines varchar(100),
     order_latlong_bussines varchar(50),
@@ -189,6 +194,7 @@ create table t_order(
     order_pryce_delivery double,
     order_pryce_total double,
     order_description text,
+    order_city varchar(20) COMMENT 'para identificar a que ciudad pertense.',
     order_status int,
     id_driver int,
     id_user int,
@@ -198,17 +204,15 @@ create table t_order(
     id_order_status int,
     FOREIGN KEY (id_user)REFERENCES t_user(user_id),
     FOREIGN KEY (id_type_payment)REFERENCES t_type_payment(type_payment_id),
-    FOREIGN KEY (id_driver)REFERENCES t_driver(driver_id),
-    FOREIGN KEY (id_cash_people)REFERENCES t_people(people_id),
     FOREIGN KEY (id_order_status)REFERENCES t_order_status(order_status_id)
 
 );
 create table t_cash_status(
-    cash_status_id int not null primary key AUTO_INCREMENT,
+    cash_status_id varchar(50) not null primary key,
     cash_status_name varchar(20) COMMENT 'PENDIENTE, PAGADO, ARQUEADO.'
 );
 create table t_cash_bussines(
-    cash_bussines_id int not null primary key AUTO_INCREMENT,
+    cash_bussines_id varchar(50) not null primary key,
     cash_bussines_correlative int,
     cash_bussines_start datetime,
     cash_bussines_end datetime,
@@ -220,7 +224,7 @@ create table t_cash_bussines(
 
 );
 create table t_order_item(
-    order_item_id int not null primary key AUTO_INCREMENT,
+    order_item_id varchar(50) not null primary key,
     order_item_product varchar(100),
     order_item_type varchar(50),
     order_item_amount double,
@@ -235,10 +239,11 @@ create table t_order_item(
     FOREIGN KEY (id_cash_bussines)REFERENCES t_cash_bussines(cash_bussines_id)
 );
 create table t_cash_driver(
-    cash_driver_id int not null primary key AUTO_INCREMENT,
+    cash_driver_id varchar(50) not null primary key,
     cash_driver_correlative int,
     cash_driver_start datetime,
     cash_driver_end datetime,
+    cash_driver_summary_all double DEFAULT 0.00,
     cash_driver_description varchar(50),
     id_cash_status int,
     id_driver int,
@@ -247,10 +252,11 @@ create table t_cash_driver(
 
 );
 create table t_cash_people(
-    cash_people_id int not null primary key AUTO_INCREMENT,
+    cash_people_id varchar(50) not null primary key,
     cash_people_correlative int,
     cash_people_start datetime,
     cash_people_end datetime,
+    cash_people_summary_all double DEFAULT 0.00,
     cash_people_description varchar(50),
     id_cash_status int,
     id_people int,
@@ -259,10 +265,18 @@ create table t_cash_people(
 
 );
 create table t_suggestions(
-    suggestion_id int not null primary key AUTO_INCREMENT,
+    suggestion_id varchar(50) not null primary key,
     suggestion_description varchar (100),
     suggestion_status int,
     id_user int,
     FOREIGN KEY (id_user)REFERENCES t_user(user_id)
 );
+create table t_city(
+    city_id varchar(50) not null primary key,
+    city_cod varchar(10),
+    city_name varchar(20),
+    city_status int,    
+);
+
+
 
