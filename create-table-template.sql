b2422e1125e4d04e95b805f334db7f6a3b3f48cc
mario <jmuspeed@gmail.com>
2021-05-21 16:40:18 -0500

    0.3

--- create-table-template.sql
+++ create-table-template.sql
@@ -10,10 +10,12 @@ create table t_user(
     user_score double NOT NULL DEFAULT 0,
     user_point double NOT NULL DEFAULT 0,
     user_gender varchar(20),
-    user_location text,
+    user_location varchar(100),
     user_birth_date date,
     user_membership_level int,
-    user_status int
+    user_status int,
+    user_date_created datetime,
+    user_date_edit datetime
 );
 create table t_document_type(
     document_type_id varchar(50) not null primary key,
@@ -27,6 +29,13 @@ create table t_vehicle_type(
     vehicle_type_image varchar(100),
     vehicle_type_status int
 );
+create table t_city(
+    city_id varchar(50) not null primary key,
+    city_cod varchar(10),
+    city_name varchar(20),
+    city_status int  
+);
+
 create table t_driver(
     driver_id varchar(50) not null primary key,
     driver_name varchar(50),
@@ -43,10 +52,14 @@ create table t_driver(
     driver_birth_date date,    
     driver_status int,
     driver_city varchar(100),
-    id_document_type int,
-    id_vehicle_type INT,
+    driver_date_created datetime,
+    driver_date_edit datetime,
+    id_document_type varchar(50) not null,
+    id_vehicle_type varchar(50) not null,
+    id_city varchar(50) not null ,
     FOREIGN KEY (id_document_type)REFERENCES t_document_type(document_type_id),
-    FOREIGN KEY (id_vehicle_type)REFERENCES t_vehicle_type(vehicle_type_id)
+    FOREIGN KEY (id_vehicle_type)REFERENCES t_vehicle_type(vehicle_type_id),
+    FOREIGN KEY (id_city)REFERENCES t_city(city_id)
 );
 create table t_soat(
     soat_id varchar(50) not null primary key,
@@ -54,7 +67,7 @@ create table t_soat(
     soat_image varchar(100),
     soat_description text,
     soat_status int,
-    id_driver int,
+    id_driver varchar(50) not null,
     FOREIGN KEY (id_driver)REFERENCES t_driver(driver_id)
 );
 create table t_license(
@@ -63,7 +76,7 @@ create table t_license(
     license_image varchar(100),
     license_description text,
     license_status int,
-    id_driver int,
+    id_driver varchar(50) not null,
     FOREIGN KEY (id_driver)REFERENCES t_driver(driver_id)
     
 );
@@ -91,7 +104,7 @@ create table t_bussines(
     bussines_date_close varchar(20),
     bussines_city varchar(100),
     bussines_score double,
-    id_bussines_category int,
+    id_bussines_category varchar(50) not null,
     FOREIGN KEY (id_bussines_category)REFERENCES t_bussines_category(bussines_category_id)
 
 );
@@ -102,12 +115,12 @@ create table t_ingredient(
     ingredient_status int
 );
 create table t_product_category(
-    t_product_category_id varchar(50) not null primary key,
-    t_product_category_name varchar(50) COMMENT ' BEBIDA, JUGO, CEVICHE',
-    t_product_category_image varchar(100),
-    t_product_category_desdcription varchar(100),
-    t_product_category_status int,
-    t_prodcut_category_order int,
+    product_category_id varchar(50) not null primary key,
+    product_category_name varchar(50) COMMENT ' BEBIDA, JUGO, CEVICHE',
+    product_category_image varchar(100),
+    product_category_desdcription varchar(100),
+    product_category_status int,
+    prodcut_category_order int
 );
 create table t_product(
     product_id varchar(50) not null primary key,
@@ -118,8 +131,8 @@ create table t_product(
     product_time varchar(20),
     product_score double,
     product_status int,
-    id_bussines int,
-    id_product_category int,
+    id_bussines varchar(50) not null,
+    id_product_category varchar(50) not null,
     FOREIGN KEY (id_bussines)REFERENCES t_bussines(bussines_id),
     FOREIGN KEY (id_product_category)REFERENCES t_product_category(product_category_id)
 
@@ -128,8 +141,8 @@ create table t_producto_ingredient(
     product_ingredient_id varchar(50) not null primary key,
     product_ingredient_porcent double,
     product_ingredient_description text,
-    id_ingredient int,
-    id_product int,
+    id_ingredient varchar(50) not null,
+    id_product varchar(50) not null,
     FOREIGN KEY (id_ingredient)REFERENCES t_ingredient(ingredient_id),
     FOREIGN KEY (id_product)REFERENCES t_product(product_id)
 );
@@ -144,13 +157,13 @@ create table t_offer(
     offer_date_end DATETIME ,
     ofeer_score double,
     offer_status int,
-    id_bussines int,   
+    id_bussines varchar(50) not null,   
     FOREIGN KEY (id_bussines)REFERENCES t_bussines(bussines_id)
 );
 create table t_product_offer(
     t_product_offer_id varchar(50) not null primary key,
-    id_offer int,
-    id_product int,
+    id_offer varchar(50) not null,
+    id_product varchar(50) not null,
     FOREIGN KEY (id_offer)REFERENCES t_offer(offer_id),
     FOREIGN KEY (id_product)REFERENCES t_product(product_id)
 );
@@ -179,7 +192,7 @@ create table t_people(
     people_image varchar(100),
     people_city varchar(100) COMMENT 'ciudad de ubicacion',
     people_status int,
-    id_role int,
+    id_role varchar(50) not null,
     FOREIGN  KEY  (id_role) REFERENCES t_role(role_id)
 
 );
@@ -194,17 +207,18 @@ create table t_order(
     order_pryce_delivery double,
     order_pryce_total double,
     order_description text,
-    order_city varchar(20) COMMENT 'para identificar a que ciudad pertense.',
     order_status int,
-    id_driver int,
-    id_user int,
-    id_type_payment int,
-    id_cash_driver int,
-    id_cash_people int,
-    id_order_status int,
+    id_city varchar(50) not null COMMENT 'para identificar a que ciudad pertense.',
+    id_driver varchar(50) default null,
+    id_cash_driver varchar(50) default  null,
+    id_user varchar(50) not null,
+    id_type_payment varchar(50) not null,    
+    id_cash_people varchar(50) default null,
+    id_order_status varchar(50) not null,
     FOREIGN KEY (id_user)REFERENCES t_user(user_id),
     FOREIGN KEY (id_type_payment)REFERENCES t_type_payment(type_payment_id),
-    FOREIGN KEY (id_order_status)REFERENCES t_order_status(order_status_id)
+    FOREIGN KEY (id_order_status)REFERENCES t_order_status(order_status_id),
+    FOREIGN KEY (id_city)REFERENCES t_city(city_id)
 
 );
 create table t_cash_status(
@@ -217,8 +231,8 @@ create table t_cash_bussines(
     cash_bussines_start datetime,
     cash_bussines_end datetime,
     cash_bussines_description varchar(50),
-    id_cash_status int,
-    id_bussines int,
+    id_cash_status varchar(50) not null,
+    id_bussines varchar(50) not null,
     FOREIGN  KEY  (id_cash_status) REFERENCES t_cash_status(cash_status_id),
     FOREIGN  KEY  (id_bussines) REFERENCES t_bussines(bussines_id)
 
@@ -231,9 +245,9 @@ create table t_order_item(
     order_item_pryce double,
     order_item_total double,
     order_item_date datetime,
-    id_order int,
-    id_bussines int,
-    id_cash_bussines int,
+    id_order varchar(50) not null,
+    id_bussines varchar(50) not null,
+    id_cash_bussines varchar(50) not null,
     FOREIGN KEY (id_order)REFERENCES t_order(order_id),
     FOREIGN KEY (id_bussines)REFERENCES t_bussines(bussines_id),
     FOREIGN KEY (id_cash_bussines)REFERENCES t_cash_bussines(cash_bussines_id)
@@ -245,8 +259,8 @@ create table t_cash_driver(
     cash_driver_end datetime,
     cash_driver_summary_all double DEFAULT 0.00,
     cash_driver_description varchar(50),
-    id_cash_status int,
-    id_driver int,
+    id_cash_status varchar(50) not null,
+    id_driver varchar(50) not null,
     FOREIGN  KEY  (id_cash_status) REFERENCES t_cash_status(cash_status_id),
     FOREIGN  KEY  (id_driver) REFERENCES t_driver(driver_id)
 
@@ -258,8 +272,8 @@ create table t_cash_people(
     cash_people_end datetime,
     cash_people_summary_all double DEFAULT 0.00,
     cash_people_description varchar(50),
-    id_cash_status int,
-    id_people int,
+    id_cash_status varchar(50) not null,
+    id_people varchar(50) not null,
     FOREIGN  KEY  (id_cash_status) REFERENCES t_cash_status(cash_status_id),
     FOREIGN  KEY  (id_people) REFERENCES t_people(people_id)
 
@@ -268,14 +282,8 @@ create table t_suggestions(
     suggestion_id varchar(50) not null primary key,
     suggestion_description varchar (100),
     suggestion_status int,
-    id_user int,
+    id_user varchar(50) not null,
     FOREIGN KEY (id_user)REFERENCES t_user(user_id)
-);
-create table t_city(
-    city_id varchar(50) not null primary key,
-    city_cod varchar(10),
-    city_name varchar(20),
-    city_status int,    
 );
 
 
